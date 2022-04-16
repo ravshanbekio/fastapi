f0baf4b407c38d60dad4cf765da2ce206a81ce22
TheRavshanDev <88030420+TheRavshanDev@users.noreply.github.com>
2022-04-16 20:50:08 +0500

    Added HTTPException to main.py file

--- blog/main.py
+++ blog/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends, status, Response
+from fastapi import FastAPI, Depends, status, Response, HTTPException
 from . import schemas, models
 from .database import engine, SessionLocal
 from sqlalchemy.orm import Session
@@ -32,6 +32,7 @@ def all_blogs(db:Session=Depends(get_db)):
 def blog(id,response:Response, db:Session=Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
     if not blog:
-        response.status_code = status.HTTP_404_NOT_FOUND
-        return {"detail":f"Blog with id {id} is not available"}
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} is not available")
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        # return {"detail":f"Blog with id {id} is not available"}
     return blog
