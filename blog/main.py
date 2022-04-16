d9b5f88c4184bb19e5c9f836d5acf5f331ee3f10
TheRavshanDev <88030420+TheRavshanDev@users.noreply.github.com>
2022-04-16 10:56:39 +0500

    Updated main.py file - added store db

--- blog/main.py
+++ blog/main.py
@@ -1,11 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from . import schemas, models
-from .database import engine
+from .database import engine, SessionLocal
+from sqlalchemy.orm import Session
 
 models.Base.metadata.create_all(engine)
 
 app = FastAPI()
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 @app.post('/blog')
-def create(request: schemas.Blog):
-    return {'title':request.title, 'body':request.text, 'author':request.author}
+def create(request: schemas.Blog, db: Session=Depends(get_db)):
+    blog_data = models.Blog(title=request.title, text=request.text, author=request.author)
+    db.add(blog_data)
+    db.commit()
+    db.refresh(blog_data)
+    return blog_data
