ec9e36975bf3d16d7ba2a72b963ecf0250bb3f8e
TheRavshanDev <88030420+TheRavshanDev@users.noreply.github.com>
2022-04-17 13:20:37 +0500

    Added PUT function to main.py, updated db

--- blog/main.py
+++ blog/main.py
@@ -42,3 +42,12 @@ def destroy(id, db:Session=Depends(get_db)):
     db.query(models.Blog).filter(models.Blog.id == id).delete(synchronize_session=False)
     db.commit()
     return 'done'
+    
+@app.put("/blog/update/{id}/", status_code=status.HTTP_202_ACCEPTED)
+def update_blog(id, request: schemas.Blog, db:Session=Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    # if not blog.first():
+    #     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with {id} id not found")
+    blog.update(request)
+    db.commit()
+    return blog
